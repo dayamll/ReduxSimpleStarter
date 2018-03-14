1cd72d4cca028e08304675f63f43829e0a296461
dayamll <dayamllerenacastro@gmail.com>
2018-03-14 09:53:08 -0500

    Añadiendo listas

--- src/components/app.js
+++ src/components/app.js
@@ -8,8 +8,15 @@ class Content extends Component {
     return (
       <div>
         <Container
+          hobbies={['leer', 'cantar', 'bailar', 'investigar']}
           header='Dayam Alisson Llerena Castro'
-          content='Dayam Alisson Llerena Castro, edad 18 años, frontend developer en laboratooria'
+          content='Front-End Developer en Laboratoria.
+          Cuento con poco más de un año en Programación Web,
+        utilizando HTML5, CSS3, JavaScript, Node.js, Babel y Frameworks.
+             Me considero extrovertida y creativa. Conocí la Programación a los 17 años
+             e investigo sobre Tecnología para aprender e ir adquiriendo nuevos conocimientos.
+             Uno de mis principales objetivos es convertirme en una UI Developer y mi mas grande sueño
+             es ser "Diseñadora de Videos Games en la empresa mas prestigiosa de Latinoamerica".'
         />
       </div>
     );
--- src/components/container.js
+++ src/components/container.js
@@ -4,11 +4,15 @@ import './style.css'
 class Container extends Component {
   render() {
     const { header, content } = this.props
+    const hobbies = this.props.hobbies.map((hobbie,index)=>{
+      return <li key={index}>{hobbie}</li>
+  })
 
     return (
       <div>
-        <header><h3>{header}</h3></header>
+        <header className='size'><h3>{header}</h3></header>
         <div>{content}</div>
+        <ul>{hobbies}</ul>
       </div>
     )
   }
